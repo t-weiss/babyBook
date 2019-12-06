7d207cc87fb3bd11da8c78cfc86e8386baf3f5dd
Tyler Weiss <tyler.k.weiss@gmail.com>
2019-12-06 15:02:35 -0500

    pulled image and working on upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BookComponent } from "./book/book.component";
+import { FormComponent } from "./form/form.component";
+import { CloudinaryModule } from "@cloudinary/angular-5.x";
+import * as Cloudinary from "cloudinary-core";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BookComponent
-  ],
+  declarations: [AppComponent, BookComponent, FormComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CloudinaryModule.forRoot(Cloudinary, { cloud_name: "babybook" })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/baby.service.ts
+++ src/app/services/baby.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class BabyService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getPhotos(): Observable<any> {
+    return this.http.get(
+      "CLOUDINARY_URL=cloudinary://525113569772349:HJt3S_txALgDBo1a-NcuIcZ_WDA@babybook"
+    );
+  }
 }
