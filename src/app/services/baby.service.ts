330238fab9574234c4f378069b7dbf3f72c36cf8
Hossayin <alihossein96@gmail.com>
2019-12-12 11:35:09 -0500

    some CSS, filled out fake account

--- src/app/services/baby.service.ts
+++ src/app/services/baby.service.ts
@@ -58,46 +58,51 @@ export class BabyService {
 
   events: any[] = [
     {
-      title: "Birth",
-      description: "Baby's birth",
-      date: new Date("2015-01-08"),
-      image: "angular_sample/h7jdjaee28qjxlvfmu7h.jpg"
+      title: "Meet baby Micheal Scott",
+      description:
+        "so happy to say that everything went super smooth. He came out smiling with a full head of hair. so blessed to have had a successful birthing!",
+      date: new Date("2017-11-04"),
+      image: "angular_sample/hocq8hn08ozs2evlnbax.jpg"
     },
     {
-      title: "started Sprinting Today",
-      description: "just got up and started to run!!",
-      date: new Date("2019-02-09"),
-      image: "angular_sample/vmdbeoztqig2bkyapiu5.jpg"
+      title: "Christmas Tiiimeee!!",
+      description:
+        "Was a great time with family and friends. Baby Mike was super happy meeting all of his out of town family for the first time. Going from grandmas arms to mine he finally fell asleep at 3 AM -_-",
+      date: new Date("2017-12-25"),
+      image: "angular_sample/fzjquo0esfkywnvye6oq.jpg"
     },
     {
-      title: "first tooth",
-      description: "billy got his first tooth",
-      date: new Date("2017-03-10"),
-      image: "angular_sample/vegdisommpwpsnc1u2z4.png"
+      title: "Lemon For the First Time",
+      description:
+        "Mike tried a lemon for the first time, you can tell how much he liked it LOOOL!!",
+      date: new Date("2018-01-10"),
+      image: "angular_sample/ilogyruyjv9kp2mv3ztd.jpg"
     },
     {
-      title: "first tooth",
-      description: "billy got his first tooth",
-      date: new Date("2017-03-10"),
-      image: "angular_sample/vegdisommpwpsnc1u2z4.png"
+      title: "First Valentines!!",
+      description:
+        "Our little cupid, So much love from Grandpa and Grandma for getting him his outfit for this Valentine's Day 💕💕❤️❤️",
+      date: new Date("2018-02-14"),
+      image: "angular_sample/tut1s0qoptufs060f8ow.jpg"
     },
     {
-      title: "first tooth",
-      description: "billy got his first tooth",
-      date: new Date("2017-03-10"),
-      image: "angular_sample/vegdisommpwpsnc1u2z4.png"
+      title: "Saint Patricks Day!🦹🏼‍♂️👯‍♀️",
+      description: "Dressed in green, my little man is saint patty ready",
+      date: new Date("2018-03-26"),
+      image: "angular_sample/lgy0qety0uau8lfariiz.jpg"
     },
     {
-      title: "first tooth",
-      description: "billy got his first tooth",
-      date: new Date("2017-03-10"),
-      image: "angular_sample/vegdisommpwpsnc1u2z4.png"
+      title: "Easter Egg Surprise",
+      description: "Lets go easter egg hunting, Mr.Scott is on the hunt!",
+      date: new Date("2018-04-12"),
+      image: "angular_sample/znuoprwfphlhih5v5l6v.jpg"
     },
     {
-      title: "first tooth",
-      description: "billy got his first tooth",
-      date: new Date("2017-03-10"),
-      image: "angular_sample/vegdisommpwpsnc1u2z4.png"
+      title: "First Tooth",
+      description:
+        "Hes growing up so fast!!! Already has  getting his first tooth 😩😭😭",
+      date: new Date("2018-08-10"),
+      image: "angular_sample/wwoij9uirzlfncp9stgm.jpg"
     }
   ];
 
